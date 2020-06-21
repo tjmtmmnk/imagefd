f23357c23f0caa5baa7c0ad5048108ebcc4b54bc
tjmtmmnk <tsujimoto027@gmail.com>
2020-06-21 21:38:42 +0900

    convert proxied image

--- magick.js
+++ magick.js
@@ -2,18 +2,24 @@ import * as Magick from 'https://knicknic.github.io/wasm-imagemagick/magickApi.j
 
 const doMagic = async () => {
     const images = Array.from(document.querySelectorAll("img")).filter(img => img.src !== '');
-    images.forEach(async image => {
-        const fetchedSourceImage = await fetch(image.src);
-        const sourceBytes = new Uint8Array(await fetchedSourceImage.arrayBuffer());
+    const proxiedImages = await Promise.all(images.map(async image => {
+        const url = 'http://localhost:8080/image?url=' + image.src;
+        const fetchedImage = await fetch(url);
+        const arrayBuffer = await fetchedImage.arrayBuffer();
+        return arrayBuffer;
+    }));
+
+    for (let i = 0; i < images.length; i++) {
+        const sourceBytes = new Uint8Array(proxiedImages[i]);
         const inputFiles = [{
             name: 'srcFile.png',
             content: sourceBytes,
         }];
-        const command = ['convert', 'srcFile.png', '-background', '#ffffff', 'outFile.png'];
+        const command = ['convert', 'srcFile.png', '-background', '#ffffff', '-alpha', 'deactivate', '-flatten', 'outFile.png'];
         const processedFiles = await Magick.Call(inputFiles, command);
         const firstOutputImage = processedFiles[0];
-        image.src = URL.createObjectURL(firstOutputImage["blob"]);
-    });
+        images[i].src = URL.createObjectURL(firstOutputImage["blob"]);
+    }
 }
 
 doMagic();
