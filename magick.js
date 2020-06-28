72ae4c496c5a9b51f42677dedcae6347a8c30327
tjmtmmnk <tsujimoto027@gmail.com>
2020-06-28 18:10:22 +0900

    error handling

--- magick.js
+++ magick.js
@@ -1,36 +1,46 @@
 import * as Magick from 'https://knicknic.github.io/wasm-imagemagick/magickApi.js';
 
 const doMagic = async () => {
-    const images = Array.from(document.querySelectorAll("img"))
-        .filter(image => {
-            const isImage = /(jpg|jpeg|gif|png)/i.test(image.src);
-            return isImage === true;
-        });
+    const images = Array.from(document.querySelectorAll("img")).filter(image => image.src !== "");
 
     const imageArrayBuffers = await Promise.all(images.map(async image => {
-        const url = 'https://imagefd.work/image';
-        const fetchedImage = await fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json; charset=UTF-8' },
-            body: JSON.stringify(
-                {
-                    url: image.src,
-                }
-            ),
-        });
-        const arrayBuffer = await fetchedImage.arrayBuffer();
-        return arrayBuffer;
+        try {
+            const url = 'https://imagefd.work/image';
+            const fetchedImage = await fetch(url, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+                body: JSON.stringify(
+                    {
+                        url: image.src,
+                    }
+                ),
+            }).catch(e => {
+                throw new Error(e);
+            });
+            if (!fetchedImage.ok) throw new Error('4xx or 5xx');
+
+            return await fetchedImage.arrayBuffer();
+        } catch (err) {
+            console.log(image.src);
+            return new ArrayBuffer(0);
+        }
     }));
 
     images.forEach(async (image, index) => {
+        if (imageArrayBuffers[index].byteLength === 0) return;
+
         const sourceBytes = new Uint8Array(imageArrayBuffers[index]);
         const inputFiles = [{
             name: 'srcFile.png',
             content: sourceBytes,
         }];
         const command = ['convert', 'srcFile.png', '-background', '#ffffff', '-alpha', 'deactivate', '-flatten', 'outFile.png'];
+
         const processedFiles = await Magick.Call(inputFiles, command);
+
         const firstOutputImage = processedFiles[0];
+        if (firstOutputImage === undefined || firstOutputImage["blob"] === undefined) return;
+        
         image.src = URL.createObjectURL(firstOutputImage["blob"]);
     });
 }
